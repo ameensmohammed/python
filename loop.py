c37c9b2642a9f2db9cf1e636e5a1e49a7f9f5502
ameensmohammed <ameensmohammed50@gmail.com>
2025-03-07 15:16:37 +0530

    while and for loop

---
+++ loop.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+n = int(input())
+i=0
+while(i<n):
+    print(i**2)
+    i+=1
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
